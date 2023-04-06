592e53acd88217c454cac5cacd624f158745f85d
Bagus Ahlul <bagus.ahlul.ft21@mail.umy.ac.id>
2023-04-06 10:21:22 +0700

    menambahkan case

--- Exercise1/Program.cs
+++ Exercise1/Program.cs
@@ -24,6 +24,89 @@ namespace Exercise1
                     char chr = Convert.ToChar(Console.ReadLine());
                     switch (chr)
                     {
+                        case 'K':
+                            {
+                                SqlConnection conn = null;
+                                string strKoneksi = "Data source = MSI\\BAGUSAHLUL; " + "initial catalog = {0}; " + "User ID = {1}; password = {2}";
+                                conn = new SqlConnection(string.Format(strKoneksi, db, user, pass));
+                                conn.Open();
+                                Console.Clear();
+                                while (true)
+                                {
+                                    try
+                                    {
+                                        Console.WriteLine("\nMenu");
+                                        Console.WriteLine("1. Melihat Seluruh Data");
+                                        Console.WriteLine("2. Tambah Data");
+                                        Console.WriteLine("3. Ubah Data");
+                                        Console.WriteLine("4. Hapus Data");
+                                        Console.WriteLine("5. Cari Data");
+                                        Console.WriteLine("6. Keluar");
+                                        Console.Write("\nEnter your choice (1-3): ");
+                                        char ch = Convert.ToChar(Console.ReadLine());
+                                        switch (ch)
+                                        {
+                                            case '1':
+                                                {
+                                                    Console.Clear();
+                                                    Console.WriteLine("DATA PASIEN\n");
+                                                    Console.WriteLine();
+                                                    pr.baca(conn);
+                                                }
+                                                break;
+                                            case '2':
+                                                {
+                                                    Console.Clear();
+                                                    Console.WriteLine("INPUT DATA PASIEN\n");
+                                                    Console.WriteLine("Masukkan ID :");
+                                                    string ID = Console.ReadLine();
+                                                    Console.WriteLine("Masukkan Nama Pasien :");
+                                                    string NmaPasien = Console.ReadLine();
+                                                    Console.WriteLine("Masukkan Alamat Pasien :");
+                                                    string Almt = Console.ReadLine();
+                                                    Console.WriteLine("Masukkan Tanggal Datang :");
+                                                    string TglDtg = Console.ReadLine();
+                                                    Console.WriteLine("Masukkan No Telepon :");
+                                                    string notlpn = Console.ReadLine();
+                                                    try
+                                                    {
+                                                        pr.insert(ID, NmaPasien, Almt, TglDtg, notlpn, conn);
+                                                    }
+                                                    catch
+                                                    {
+                                                        Console.WriteLine("\nAnda tidak memiliki " + "akses untuk menambah data");
+                                                    }
+                                                }
+                                                break;
+                                            case '3':
+                                                conn.Close();
+                                                return;
+                                            default:
+                                                {
+                                                    Console.Clear();
+                                                    Console.WriteLine("\nInvalid option");
+                                                }
+                                                break;
+                                        }
+                                    }
+                                    catch
+                                    {
+                                        Console.WriteLine("\nCheck for the value entered.");
+                                    }
+                                }
+                            }
+                        default:
+                            {
+                                Console.WriteLine("\nInvalid option");
+                            }
+                            break;
                     }
+                }
+
+
+
+                            
+
+                    
     }
 }
