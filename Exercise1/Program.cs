edc670e0fe9120c0d1e9434d141f1bef6e962835
Bagus Ahlul <bagus.ahlul.ft21@mail.umy.ac.id>
2023-04-06 09:57:30 +0700

    Add project files.

---
+++ Exercise1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
